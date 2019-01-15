3cfc42e2ad7e996622723477b47e4edd9382b1ed
Keilan Jackson <keilan@bluematador.com>
2019-01-14 17:55:06 -0700

    Update cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support@bluematador.com'
 license 'MIT License'
 description 'Installs/Configures bluematador_agent'
 long_description 'Installs/Configures bluematador_agent'
-version '1.0.0'
+version '1.0.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
